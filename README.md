fb871e35349cc180b162f49570e297b8e3659c1b
Chandon Itburrun <chandon@Chandons-Air.lan>
2025-04-15 16:59:23 +0100

    nearly completed all the table creation

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # NC News Seeding
 
-- Instructions for this sprint can be found at https://l2c.northcoders.com/courses/be/seeding-nc-news
+To use the database you will need to set up the environment variables.
+
+2 files will need to be added, they will need to be .env. then the relevant file name as this will allow you to access our testing or development data.
+
+Please kindly complete this task with the relevant name for the files depending on whether you need a test or a development file, an example would be **.env.test** or **.env.development** 
+
+The clue to your set up is in the example.
+
+Best of luck.
--- db/seeds/seed.js
+++ db/seeds/seed.js
@@ -1,6 +1,52 @@
 const db = require("../connection")
 
 const seed = ({ topicData, userData, articleData, commentData }) => {
-  return db.query(); //<< write your first query in here.
-};
+  return db
+  .query(`DROP TABLE IF EXISTS comments`)
+  .then(() => {
+   return db.query(`DROP TABLE IF EXISTS articles`)
+  })
+  .then(() => {
+   return db.query(`DROP TABLE IF EXISTS users`)
+  })
+  .then(() => {
+   return db.query(`DROP TABLE IF EXISTS topics`)
+  })
+  .then(() => {
+    return db.query(`CREATE TABLE topics (
+      slug TEXT PRIMARY KEY,
+      description TEXT NOT NULL,
+      img_url TEXT
+    )`)
+  })
+  .then(() => {
+    return db.query(`CREATE TABLE users(
+      username TEXT PRIMARY KEY,
+      name VARCHAR(100) NOT NULL,
+      avatar_url TEXT
+    )`)
+  }) 
+  .then(() => {
+    return db.query(`CREATE TABLE articles(
+      article_id SERIAL PRIMARY KEY,
+      title VARCHAR(100) NOT NULL,
+      topic TEXT REFERENCES topics(slug),
+      author TEXT REFERENCES users(username),
+      body TEXT,
+      created_at DATE,
+      votes INT DEFAULT 0,
+      article_img_url TEXT
+    )`)
+  }) 
+  .then(() => {
+    return db.query(`CREATE TABLE comments(
+      comment_id SERIAL PRIMARY KEY,
+      article_id INT REFERENCES articles(article_id),
+      body TEXT,
+      votes INT DEFAULT 0,
+      author VARCHAR REFERENCES users(username),
+      created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    )`)
+  });
+}
 module.exports = seed;
