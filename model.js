2dcad2101680b9a9df2bd43f87f37080cc40a4ce
Chandon Itburrun <chandon@Chandons-Air.lan>
2025-04-29 15:06:03 +0100

    Completed question 3

--- model.js
+++ model.js
@@ -6,3 +6,10 @@ exports.fetchTopics = () => {
       return result.rows;
     });
   };
+exports.fetchArticleId = (article_id) => {
+    return db.query("SELECT * FROM articles WHERE article_id = $1;", [article_id])
+    .then((result) => {
+        return result.rows[0];
+    })
+}
+  
